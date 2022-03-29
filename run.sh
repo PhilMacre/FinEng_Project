10845eb8c51c56d2fe494916c1925a21cf327227
gregoirelatour <gregoire.latour@edu.devinci.fr>
2022-03-29 13:44:58 +0000

    maj run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
-[ -f .env ] && echo "This file exists!"
+[ -d ".gitignore/my_env" ] || python3 -m venv .gitignore/my_env ; source .gitignore/my_env/bin/activate ; python3 -m pip install -r .gitignore/my_env/bin/requirements.txt
+source .gitignore/my_env/bin/activate && source .gitignore/my_env/bin/activate
+python3 main.py
+
+
 
