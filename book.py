f2a10943103b94084abe3be4f72810f21ef7f869
quentin <ulane.quentin@gmail.com>
2022-03-29 21:53:33 +0200

    new structure Book & Order (Adding historical deal)

--- book.py
+++ book.py
@@ -1,73 +1,110 @@
-import bisect
-from functools import total_ordering
+from order import Order
+from deal import Deal
 
-class Order:
-    identi=0
-    def __init__(self, quantity, price, buy=True,id=0):
-        self.quantity = quantity
-        self.price = price
-        self.buy = True  #quand on insert sell on change le self.buy to false
-        Order.identi+=1
-        self.id = Order.identi
-    def  __str__(self):
-        if self.buy == True:
-            return f"BUY {self.quantity}@{self.price} id={self.id}" #pas besoin de mettre l'id et le nom du book, on le fera pour override book.__str__
-        else :
-            return f"SELL {self.quantity}@{self.price} id={self.id} "
+import pandas as pd
 
-    def __eq__(self, other):
-        return other and self.quantity == other.quantity and self.price==other.price
-
-    def __lt__(self,other):
-        return other and self.price < other.price
-    def __repr__(self): # unambiguous representation of the object
-        return f"Order({self.quantity}, {self.price})"
 
 class Book:
-    def __init__(self,NAME):
-        self.NAME = NAME
-        self.buy_orders=[]
-        self.sell_orders=[]
-
-    def __str__(self):
-        a=f"Book on {self.NAME} \n \t "
-        print(a)
-        for i in range(len(self.sell_orders)-1): 
-            print(f"{self.sell_orders[i].__str__}")
-        for j in range(len(self.buy_orders)-1):
-            print(f"{self.buy_orders[i].__str__}")
+    def __init__(self, name='Default order book', buy_orders=[], sell_orders=[], execute_deals=[]):
+        
+        self._name = name
+        self._buy_orders = buy_orders
+        self._sell_orders = sell_orders
+        self._execute_deals = execute_deals
+        
         
 
-    def insert_sell(self,quantity,price):
-        sell_order=Order(quantity,price,False)
-        print(f"----- Insert {sell_order.__str__()} on {self.NAME}")
-        self.sell_orders.sort(key=lambda x: x.price*x.id,reverse=False)#sell donc le plus petit prix en premier mais pour s'épargner l'ordre qui est arrivé en premier aura son produit P0*id plus petit, plus besoin de comparer les id
-        while len(self.buy_orders)!=0 and self.buy_orders[0].price >= sell_order.price and sell_order.quantity > 0:
-            if sell_order.quantity < self.buy_orders[0].quantity:
-                self.buy_orders[0].quantity-=sell_order.quantity
-            else : 
-                sell_order.quantity -= self.buy_orders[0].quantity
-                self.buy_orders=self.buy_orders[1:]
-        bisect.insort(self.sell_orders,sell_order)
-        print(self.__str__())
+    def insert_sell(self, quantity, price):
         
+        sell_order = Order(quantity, price, type_of_order='sell')
 
-    def insert_buy(self,quantity,price):
-        buy_order=Order(quantity,price,True)
-        print(f"----- Insert {buy_order.__str__()} on {self.NAME}")
+        if quantity != 0:
+            self._sell_orders.append(sell_order)
+            self._sell_orders.sort()
+            
+        while len(self._buy_orders) == 0 and self._buy_orders[0].get_price() >= sell_order.get_price() and sell_order.get_quantity() > 0:
+
+           
+            if sell_order.get_quantity() > self._buy_orders[0].get_quantity():
+                deal = Deal(self._buy_orders[0].get_quantity(), self._buy_orders[0].get_price(), self._name)
+                self._execute_deals.append(deal)
+
+                new_qty = sell_order.get_quantity() - self._buy_orders[0].get_quantity()
+
+               
+                self._buy_orders.remove(self._buy_orders[0])
+
+                sell_order.set_quantity(new_qty)
+                
+
+         
+            else:
+                deal = Deal(sell_order.get_quantity(), self._buy_orders[0].get_price(), self._name)
+                self._execute_deals.append(deal)
+
+               
+                self._buy_orders[0].set_quantity(self._buy_orders[0].get_quantity() - sell_order.get_quantity())
+
+                sell_order.set_qquantity(0)
+
+                if self._buy_orders[0].get_quantity() == 0:
+                    
+                    self._buy_orders.remove(self._buy_orders[0])
+
+                self._sell_orders.remove(self._sell_orders[0])
+
+
+        return None
+
+    def insert_buy(self, quantity, price):
+       
+        buy_order = Order(quantity, price, type_of_order='buy')
+
+        if quantity != 0:
+            self._buy_orders.append(buy_order)
+            self._buy_orders.sort()
+            self._buy_orders.reverse()
+           
+        while len(self._sell_orders) != 0 and self._sell_orders[
+            0].get_price() <= buy_order.get_price() and buy_order.get_quantity() > 0:
+
+         
+            if buy_order.get_quantity() > self._sell_orders[0].get_quantity():
+                
+                deal = Deal(self._sell_orders[0].get_quantity(), self._sell_orders[0].get_price(), self._name)
+                
+                self._execute_deals.append(deal)
+
+                new_qty = buy_order.get_quantity() - self._sell_orders[0].get_quantity()
+               
+                self._sell_orders.remove(self._sell_orders[0])
+
+                buy_order.set_qty(new_qty)
+               
+
+          
+            else:
+                deal = Deal(buy_order.get_quantity(), self._sell_orders[0].get_price(), self._name)
+                
+                self._execute_deals.append(deal)
+
+                self._sell_orders[0].set_quantity(self._sell_orders[0].get_quantity() - buy_order.get_quantity())
+
+                buy_order.set_quantity(0)
+
+                self._buy_orders.remove(self._buy_orders[0])
+
+                if self._sell_orders[0].get_quantity() == 0:
+                    
+                    self._sell_orders.remove(self._sell_orders[0])
+
+        return None
+
+    def insert_deals(self, deal):
         
-        self.buy_orders.sort(key=lambda x: x.price*x.id,reverse=True) #buy donc le plus élevé en premier
-        while len(self.sell_orders)!=0 and self.sell_orders[0].price <= buy_order.price and buy_order.quantity > 0:
-            if buy_order.quantity<self.sell_orders[0].quantity: #Ordre cmplet
-                self.sell_orders[0].quantity-=buy_order.quantity
-            else : #Ordre partiel
-                buy_order.quantity-=self.sell_orders[0].quantity
-                self.sell_orders=self.sell_orders[1:]
-        bisect.insort(self.buy_orders,buy_order)
-        #print(self.__str__())
+        self._execute_deals.append(deal)
+        return None
 
+    
 
-
-
-
-
+    
---
+++ order.py
@@ -0,0 +1,44 @@
+import itertools
+
+
+class Order:
+    nb_of_id = itertools.count(0) 
+    
+    
+    
+    def __init__(self, quantity, price, type_of_order):
+        
+        self._quantity = quantity
+        self._price = price
+        self._type = type_of_order.upper()
+        self._id = self.nb_of_id
+        
+        
+        
+    def __str__(self): 
+        return self._type.upper() + " %s@%s id=%s" % (self._quantity, self._price, self._id)
+
+    def __eq__(self, other):
+        
+        return (self._quantity, self._price) == (other._quantity, other._price)
+
+    def __lt__(self, other):
+        
+        return self._price <= other._price
+
+    def get_id(self):
+        
+        return self._id
+
+    def get_quantity(self):
+        
+        return self._quantity
+
+    def get_price(self):
+       
+        return self._price
+
+    def set_quantity(self, quantity):
+        
+        self._quantity = quantity
+
